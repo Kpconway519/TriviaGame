d0ca4a8b22f42a31f5684aa9579e1fe888eb63a2
Kevin519 <kpconway519@gmail.com>
2018-10-27 02:09:05 -0400

    made up for some sick days with some good progress. just a few more steps until completion. Looking to finish after work tomorrow.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,30 +1,116 @@
+//stopping point. I still need to:
+//1. Increment the scoring system
+//2. figure out how to go through the different questions and go to the final screen once all of them are done.
+//3. do my afterQuestion screen with the picture and the text. 
+//I think I can just do a bunch of true/false switches for most of the control stuff. 
+//4. make a mouseover function which lights up the answer when i hover over it.
+//5. make a system which can tell if a selected answer is correct or not.
+
+
+
+
+
 //this is the array which holds all my answers, and other useful global variables
 var triviaObject = {
 questions: ["Which of these states extends the farthest west?", "Which of these former US presidents almost took part in a duel with broadswords (before the other guy backed out)?", "Benjamin Franklin thought the Bald Eagle was dishonorable because it is a scavenger and sometimes steals food from other birds. Which bird did he want to be the national bird instead?", "The country of Sri Lanka was known by this name before 1972:", "This ancient Roman ruler was once kidnapped by pirates. (Caesar) (I should include the clip from Pirates of the Caribbean--thats got to be the best pirate I've ever seen"],
-//for now, make the first question in each array the correct one.
-//is there a better, more scalable way to make these question arrays?
-q1answers: ["Virginia", "West Virginia", "Maine", "South Carolina"],
-q2answers: ["Abe Lincoln", "Teddy Roosevelt", "Thomas Jefferson", "Jimmy Carter"],
-q3answers: ["Turkey", "Sparrow", "Hummingbird", "Peregrine Falcon"],
-q4answers: ["Ceylon", "Myanmar", "Siam", "Thucydides"],
-q5answers: ["Julius Caesar", "Caligula", "Vespasian", "Hadrian"],
+answers: [
+    ["Virginia", "West Virginia", "Maine", "South Carolina"],
+    ["Abe Lincoln", "Teddy Roosevelt", "Thomas Jefferson", "Jimmy Carter"],
+    ["Turkey", "Sparrow", "Hummingbird", "Peregrine Falcon"],
+    ["Ceylon", "Myanmar", "Siam", "Thucydides"],
+    ["Julius Caesar", "Caligula", "Vespasian", "Hadrian"],
+]
 //questionPictures: //Put the pictures here, I need to figure out exactly how to do that.
 };
 
-var correctAnswers;
-var wrongAnswers;
-var unanswered;
+var correctAnswers = 0;
+var wrongAnswers = 0;
+var unanswered = 0;
 
 //===========================================================
-
+//this section is for the start screen with a button to begin the game. 
+//current-question.append button
+//button runs the functions to run the game.
+function beginGame() {
+$("#current-question").append("<input type='button' id='start-game' value='Start Game!'>")
+$("#start-game").on("click", function() {
+    currentQuestion();
+    makeAnswers();
+    //start timer as well.
+    run()
+})
+}
+beginGame()
+//========================================================
 //This is the area for the function which handles the current question.
-var questionTime = (currentQuestion, 1000 * 30);
+
+//mini section for the timer=-===============-=-==-=-=-=-=-=-=-=-=-=-=-=-==-=-=-
+var timeNumber = 30;
+var intervalId;
+function run() {
+    clearInterval(intervalId);
+    intervalId = setInterval(decrement, 1000);
+    
+};
+
+function decrement() {
+    timeNumber--;
+    $("#time-remaining").text("Time Remaining: " + timeNumber);
+    if (timeNumber === 0) {
+        unanswered++;
+        //replace stop with go to next thing function
+        stop();
+        alert("out of time");
+        console.log(unanswered);
+        resetClock()
+    }
+};
+
+function stop() {
+    clearInterval(intervalId);
+}
+
+function resetClock() {
+    timeNumber = 30;
+    run()
+}
+//will run when the time is right.
+
+
+
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+//make a section for the question selector. first I am going to have a var questionSelector. it is going to be tied to the index of triviaObject.questions. when the start button is pressed at the beginning of the game, or Whenever a question is over with, currentQuestion() will run with the new question. whenever it is out of questions, it will display the final results screen. 
+
+var questionSelector = 0;
 
 function currentQuestion() {
-//make 4 new div cards with the different answers in them.
+    $("#current-question").text("");
+    $("#current-question").text(triviaObject.questions[questionSelector]);
 //make the divs change color when you mouse over them.
-//When the correct question is selected, 
+//When the correct question is selected, go to the next question
 };
+
+function makeAnswers() {
+    //take the triviaObject.answers[questionSelector] and for that item, loop through the items inside and make divs out of them.
+    for (i = 0; i < triviaObject.answers[questionSelector].length; i++) {
+    var answers = $("<div class='card answer-option'>").text(triviaObject.answers[questionSelector] [i]);
+    $("#current-question").append(answers);
+    }
+
+    $(".answer-option").on("click", function() {
+        alert("you clicked an answer");
+        deleteChildren()
+    })
+}
+
+//need to make a click function for the answers. start with on click alert("you clicked an answer")
+
+
+function deleteChildren() {
+    var questionParent = $("#current-question");
+    questionParent.html("");
+}
+
 //============================================================
 
 //this is the section for what happens after the player selects a question, right or wrong.
@@ -47,9 +133,6 @@ var finalScreen = function() {
 //============================================================
 
 //TODO: 
-//1. How do I produce the answers? I could procedurally generate them with javascript. I think that would be the best technique to learn to do.
-//2. I need to set up a system where the user starts by looking at a start screen with a button to start the game.
-//3. use my skills from activity 10 to produce a 30 second timer function and display in the right place
 //4. need to make it so when I mouse over the things, it lights up the answer selections
 //5. make the picture display after user selects an answer. try to make this scalable and not specific to just that one thing.
 
@@ -63,4 +146,5 @@ var finalScreen = function() {
 
 
 //first session: Sunday 10/21 did 3 hours.
+//second session: Friday 10/27 did 3 more hours.
 
